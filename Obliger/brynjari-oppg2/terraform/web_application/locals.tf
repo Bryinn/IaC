32c3466321db9a7afadb8caa1c55468feec86139
Bryinn <Bryinn@live.no>
2024-11-07 15:17:45 +0100

    done

--- Obliger/brynjari-oppg2/terraform/web_application/locals.tf
+++ Obliger/brynjari-oppg2/terraform/web_application/locals.tf
@@ -1,6 +1,18 @@
 locals {
   workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
 
-  rg_name = "${var.rg_name}-${local.workspace_suffix}"
+  common_instance_name = "webshop"
+  location             = "West Europe"
+  common_tags = {
+    project      = "website"
+    owner        = "brynjari"
+    billing_code = "abc123457test${local.workspace_suffix}"
+  }
+  expiration_date = "2026-12-31T00:00:00Z"
+
+
+
+
+  rg_name = "${local.common_instance_name}-${local.workspace_suffix}"
 
 }
